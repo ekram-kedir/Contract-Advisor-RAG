d62b61beeb63e1d37de64c433922f64aa71f89af
ekram <ekruiwc2020@gmail.com>
2024-02-22 14:50:27 +0300

    feat: chatbot

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+);
+
+
