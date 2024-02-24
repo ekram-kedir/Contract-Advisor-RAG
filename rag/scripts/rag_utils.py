723589c066fb0ab795f9a2c344f258538252499d
ekram <ekruiwc2020@gmail.com>
2024-02-24 22:07:28 +0300

    feat: cleanup code

--- rag/scripts/rag_utils.py
+++ rag/scripts/rag_utils.py
@@ -1,17 +1,10 @@
-import json
 import logging
 import weaviate
 from typing import List,  Union
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter  
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.schema.output_parser import StrOutputParser
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Weaviate
-# from weaviate import EmbeddedOptions
-from datasets import Dataset
 from dotenv import load_dotenv,find_dotenv
 from weaviate.embedded import EmbeddedOptions
 
@@ -64,29 +57,29 @@ def data_loader(file_path: str, chunk_size: int = 500, chunk_overlap: int = 50) 
     
 
 def extract_questions_and_groundtruth(file_path):
-  """
-  Extracts questions and ground truth answers from a document.
+    """
+    Extracts questions and ground truth answers from a document.
 
-  Args:
-    document: A string containing the text document.
+    Args:
+        document: A string containing the text document.
 
-  Returns:
-    A list of dictionaries, where each dictionary contains:
-      - question: The extracted question string.
-      - ground_truth_answer: The corresponding ground truth answer string.
-  """
-  with open(file_path, "r") as file:
-    document = file.read()
-    
-  eval_questions = []
-  eval_answers = []
-  for line in document.splitlines():
-    if line.startswith("Q"):
-      eval_questions.append(line[3:])
-    elif line.startswith("A"):
-      eval_answers.append([line[3:]])
+    Returns:
+        A list of dictionaries, where each dictionary contains:
+        - question: The extracted question string.
+        - ground_truth_answer: The corresponding ground truth answer string.
+    """
+    with open(file_path, "r") as file:
+        document = file.read()
+        
+    eval_questions = []
+    eval_answers = []
+    for line in document.splitlines():
+        if line.startswith("Q"):
+            eval_questions.append(line[3:])
+        elif line.startswith("A"):
+            eval_answers.append([line[3:]])
 
-  return eval_questions,eval_answers
+    return eval_questions,eval_answers
 
 def create_retriever(chunks):
     try:
@@ -113,85 +106,3 @@ def create_retriever(chunks):
         print(f"An unexpected error occurred: {e}")
         return None
 
-def create_langchain_pipeline(retriever, template, temperature=0):
-    try:
-        # Define LLM
-        llm = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=temperature)
-
-        # Define prompt template
-        
-        prompt = ChatPromptTemplate.from_template(template)
-
-        # Setup RAG pipeline
-        rag_chain = (
-            {"context": retriever,  "question": RunnablePassthrough()} 
-            | prompt 
-            | llm
-            | StrOutputParser() 
-        )
-
-        print("langchain with rag pipeline created successfully.")
-        return rag_chain
-
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return None 
-    
-    
-
-def generate_testcase_and_context(questions, ground_truths, retriever, rag_chain):
-    try:
-        answers = []
-        contexts = []
-
-        # Inference
-        for query in questions:
-
-            answers.append(rag_chain.invoke(query))
-            contexts.append([docs.page_content for docs in retriever.get_relevant_documents(query)])
-
-            
-        data = {
-            "question": questions, # list 
-            "answer": answers, # list
-            "contexts": contexts, # list list
-            "ground_truths": ground_truths # list Lists
-        }
-
-
-        # Convert dict to dataset
-        dataset = Dataset.from_dict(data) 
-        print("automatic evaluation data generated succesfully.")
-
-        return  dataset
-    
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return None 
-
-
-def get_generated_prompt_with_evaulation(question):
-    try:
-        chunks = data_loader()
-        retriever = create_retriever(chunks)
-
-        prompt_template = load_file('../prompts/prompt-generation-prompt.txt')
-        evaluation_tempate = load_file('../prompts/evaluation-data-generation.txt')
-
-
-        prompt_rag_chain = create_langchain_pipeline(retriever, prompt_template)
-        evaulation_rag_chain = create_langchain_pipeline(retriever, evaluation_tempate, temperature=0.2)
-
-
-        generated_prompts = prompt_rag_chain.invoke(question)
-        prompt_list  = json.loads(generated_prompts)
-
-        questions = [item['prompt'] for item in prompt_list]
-        ground_truths = [[item['ground_truth']] for item in prompt_list]
-
-        response = generate_testcase_and_context(questions, ground_truths, retriever, evaulation_rag_chain)
-        return response
-    
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return None 
